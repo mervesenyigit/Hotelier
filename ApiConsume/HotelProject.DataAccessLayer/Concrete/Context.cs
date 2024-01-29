7e07ef60dc62e1cc2273c8801288ad57e545c5a4
MERVE ŞENYİĞİT <merve.senyigit@outlook.com>
2024-01-29 22:36:31 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+    public class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=Merve\\SQLEXPRESS;Initial Catalog=Hotelier;User ID=sa;Password=0258;Connect Timeout=60;MultipleActiveResultSets=True;TrustServerCertificate=true;");
+        }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+   
+        
+    }
+}
